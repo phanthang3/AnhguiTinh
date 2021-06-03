0ec97d9a4813f3a79bb6e5a262b89b2d799330ca
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2021-06-03 10:30:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Vân Anh à!',
+    introDesc: `Chuyên xe nào mà chẳng có điểm dừng
+    Theo đuổi em thì anh chẳng muốn ngưng
+    Thanh xuân này...
+    Có em sẽ thật đẹp`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Vân Anh có iu anh không?',
+    desc: 'Anh hỏi lần nữa em có iu anh không ',
+    btnYes: 'Dạ....Tất nhiên là cóa',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Em có nhiều người theo đuổi mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều',
+    messDesc: 'Thực sự chỉ muốn nói là yêu Vân Anh nhiều hơn hôm qua.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
