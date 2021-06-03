a53713875f08139928e1dba83523cd920cf8893e
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2021-06-03 15:00:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Vân Anh à!',
-    introDesc: `Chuyên xe nào mà chẳng có điểm dừng
-    Theo đuổi em thì anh chẳng muốn ngưng
-    Thanh xuân này...
-    Có em sẽ thật đẹp`,
-    btnIntro: 'hihi',
+    introDesc: `Chuyến xe nào mà chẳng có điểm dừng,
+    Theo đuổi em thì anh chẳng muốn ngưng,
+    Chí Phèo lúc say nhớ về Thị Nở,
+    Còn Thắng lúc buồn thì nhớ về Vân Anh`,
+    btnIntro: 'Ấn đây nha VAnh',
     title: 'Vân Anh có iu anh không?',
-    desc: 'Anh hỏi lần nữa em có iu anh không ',
-    btnYes: 'Dạ....Tất nhiên là cóa',
+    desc: 'Anh hỏi lần nữa em có iu anh không? ',
+    btnYes: 'Dạ....Tất nhiên là cóa-.-',
     btnNo: 'Không, Anh đừng ảo tưởng anh Thắng à -.-',
     question:'Em có nhiều người theo đuổi mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Em ấn vào đây tiếp đi <3',
+    reply: 'Vì đã lỡ yêu anh rồi-.- <33333333',
     mess: 'Anh biết mà. iu em nhiều nhiều',
     messDesc: 'Thực sự chỉ muốn nói là iu Vân Anh nhiều hơn hôm qua.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Nếu tò mò thì nhấp vào <3',
     messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
 }
