53869a1c2cbde6253401c76ca582bf32a13adeb0
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2021-06-03 10:45:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Vân Anh có iu anh không?',
     desc: 'Anh hỏi lần nữa em có iu anh không ',
     btnYes: 'Dạ....Tất nhiên là cóa',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Anh đừng ảo tưởng anh Thắng à -.-',
     question:'Em có nhiều người theo đuổi mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà. Yêu em nhiều nhiều',
-    messDesc: 'Thực sự chỉ muốn nói là yêu Vân Anh nhiều hơn hôm qua.',
+    mess: 'Anh biết mà. iu em nhiều nhiều',
+    messDesc: 'Thực sự chỉ muốn nói là iu Vân Anh nhiều hơn hôm qua.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
 }
