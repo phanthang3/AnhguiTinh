8a0ad69a9b14310957c87edb1fdc11a60058304e
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2022-06-07 13:06:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,11 @@ const CONFIG = {
     desc: 'Anh hỏi lần nữa em có iu anh không? ',
     btnYes: 'Dạ....-.-',
     btnNo: 'Không, Anh đừng ảo tưởng-.-',
+    question:'Em có biết là anh say em lắm rồi không',
+    btnReply: 'Em trả lời câu trên rồi ấn vô đây nhó <3',
+    reply: 'hmmm-.-,
     mess: 'Anh biết mà',
     messDesc: 'Thực sự chỉ muốn nói là iu Ngọc Mai nhiều hơn hôm qua.',
     btnAccept: 'Nếu em tò mò thì nhấp vào',
     messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
+}
