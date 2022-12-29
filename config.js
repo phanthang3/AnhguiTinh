18229cf18cc8cdca04bfd35e171ccc52eeae3e99
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2022-12-29 18:38:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    introTitle: 'Quỳnh Linh à!',
+    introTitle: 'Ngọc Mai à!',
     introDesc: `Chuyến xe mô mà nỏ có điểm dừng,
     Theo đuổi em thì anh nỏ muốn ngưng,
     Chí Phèo lúc say nhớ về Thị Nở,
-    Còn anh khi mô  cụng nhớ Quỳnh Linh `,
+    Còn anh khi mô  cụng nhớ Ngọc Mai`,
     btnIntro: 'Em ấn vô đây nha',
-    title: 'Quỳnh Linh muốn mần con du mẹ anh k?',
+    title: 'Mai muốn mần con du mẹ anh k?',
     desc: 'Anh hỏi lần nưạ em có iu anh không? ',
     btnYes: 'Dạaa...-.-',
     btnNo: 'Không, Anh đừng ảo tưởng-.-',
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Em trả lời câu trên rồi ấn vô đây nhó <3',
     reply: 'Dạaaaaaaa',
     mess: 'Anh biết mà',
-    messDesc: 'Thực sự chỉ muốn nói là iu Quỳnh Linh nhiều hơn bưạ qua.',
+    messDesc: 'Thực sự chỉ muốn nói là iu Ngọc Mai nhiều hơn bưạ qua.',
     btnAccept: 'Nếu em muốn biết điều anh nói thì ấn vô đây nha',
     messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
 }
