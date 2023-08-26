d7f9c978df8e534a4446933ee5293975022ff9b7
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2023-08-26 11:17:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Chí Phèo lúc say nhớ về Thị Nở,
     Còn anh khi mô cụng nhớ Lan Hương`,
     btnIntro: 'Em ấn vô đây nha',
-    title: 'Lan Hương có thích anh ?',
+    title: 'Lan Hương có thích anh khong?',
     desc: 'Anh hỏi lần nưạ em có iu anh không? ',
     btnYes: 'Dạaa...-.-',
     btnNo: 'Không, Anh đừng ảo tưởng-.-',
     question:'Em.có biết là anh say em lắm rồi không',
     btnReply: 'Em trả lời câu trên rồi ấn vô đây nhó <3',
-    reply: 'Dạaaaaaaaa',
+    reply: 'Dạaaaaaaaaaaaaaaaaaaaaaaa',
     mess: 'Anh biết mà',
-    messDesc: 'Thực sự chỉ muốn nói là iu Ngọc Mai nhiều hơn bưạ qua.',
+    messDesc: 'Thực sự chỉ muốn nói là iu Lan Hươnng nhiều hơn bưạ qua.',
     btnAccept: 'Nếu em muốn biết điều anh nói thì ấn vô đây nha',
     messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
 }
