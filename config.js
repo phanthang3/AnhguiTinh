815de3b576158a5e70cca2f7848fa02a7f56f5cf
phanthang3 <65223469+phanthang3@users.noreply.github.com>
2024-02-20 11:38:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Embe Tình à!',
+    introTitle: 'Embe Hường à!',
     introDesc: `Chuyến xe mô mà nỏ có điểm dừng,
     Theo đuổi em thì anh nỏ muốn ngưng,
     Chí Phèo lúc say nhớ về Thị Nở,
     Còn anh khi mô cụng nhớ về Em`,
-    btnIntro: 'Em ấn vô đây nha',
+    btnIntro: 'Em ấn vào đây nha',
     title: 'Em có thích anh khong?',
     desc: 'Anh hỏi lần nưạ em có iu anh không? ',
     btnYes: 'Dạaa...-.-',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà',
     messDesc: 'Quá là tuyệt vời.',
     btnAccept: 'Nếu em muốn biết điều anh nói thì ấn vô đây nha',
-    messLink: 'https://www.facebook.com/watch/?ref=saved&v=1841772742667641'
+    messLink: 'https://www.facebook.com/share/v/mogY6FAsjpEu8GXJ/?mibextid=FQVVTg'
 }
